36430b8130b60695c15d8fc4e20ad39f3422cdd9
pushpank08 <pushpankgarg5660@gmail.com>
2026-01-17 12:04:25 +0530

    Create sofc_model.py

---
+++ src/sofc_model.py
@@ -0,0 +1,15 @@
+SOFC-System-Modeling/
+│
+├── src/
+│   ├── sofc_model.py
+│   ├── losses.py
+│
+├── notebooks/
+│   └── SOFC_System_Model.ipynb
+│
+├── results/
+│   ├── polarization_curve.png
+│   ├── power_density.png
+│   └── efficiency.png
+│
+└── README.md
